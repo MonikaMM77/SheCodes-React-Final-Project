ca28c41433cdbef95bbc62fa25169e8eb27fd3a5
Monika Mitka <monikacodes@gmail.com>
2021-01-10 15:16:13 +0100

    add GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Monika Mitka and is <a href="https://github.com/MonikaMM77/SheCodes-React-Final-Project" target="_blank">open-sourced on GitHub</a>
+      </footer>
+      
     </div>
   );
 }
