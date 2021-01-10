2021832247ab24cfa0c6c101777337de0f1b1b37
Monika Mitka <monikacodes@gmail.com>
2021-01-10 15:46:34 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Monika Mitka and is <a href="https://github.com/MonikaMM77/SheCodes-React-Final-Project" target="_blank">open-sourced on GitHub</a>
-      </footer>
-      
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by Monika Mitka and is <a href="https://github.com/MonikaMM77/SheCodes-React-Final-Project" target="_blank">open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
