7e75b5344fce295909536051d3bf52c9ab4b60fe
Monika Mitka <monikacodes@gmail.com>
2021-01-15 15:46:42 +0100

    add WeatherIcon component, modify css

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
 			description: response.data.weather[0].description,
 			wind: response.data.wind.speed,
 			humidity: response.data.main.humidity,
-			iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+			icon: response.data.weather[0].icon
 		});
 	}
 
