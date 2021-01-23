60188fec6304bcef999423a0ba81d669877680cc
Monika Mitka <monikacodes@gmail.com>
2021-01-23 17:58:00 +0100

    replace http with https in apiUrlin Weather.js component

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
 
 	function search(city) {
 		const apiKey = "30d51b8f5d573674a85c2b8f5f80916d";
-		let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 		axios.get(apiUrl).then(handleResponse);
 	}
 
